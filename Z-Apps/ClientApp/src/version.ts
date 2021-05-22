e9cad13a1933ee9688d5bb58240f95d896b005d5
Kosuke_Zaizen <kosukezaizen@yahoo.co.jp>
2021-05-23 00:21:08 +0900

    Folktalesのテーブル構造の変更に伴う修正

--- Z-Apps/ClientApp/src/version.ts
+++ Z-Apps/ClientApp/src/version.ts
@@ -1,1 +1,1 @@
-export const APP_VERSION: number = 462;
+export const APP_VERSION: number = 463;
--- Z-Apps/Models/Stories/Stories/StoryManager.cs
+++ Z-Apps/Models/Stories/Stories/StoryManager.cs
@@ -3,25 +3,31 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 
-namespace Z_Apps.Models.Stories.Stories {
-    public class StoryManager {
+namespace Z_Apps.Models.Stories.Stories
+{
+    public class StoryManager
+    {
         private readonly DBCon Con;
-        public StoryManager(DBCon con) {
+        public StoryManager(DBCon con)
+        {
             Con = con;
         }
 
-        public IEnumerable<Story> GetAllStories() {
+        public IEnumerable<Story> GetAllStories()
+        {
             //SQL文作成
-            string sql = "";
-            sql += " select * from tblStoryMst";
-            sql += " order by [Order] desc";
+            string sql = @"
+            select * from tblStoryMst
+            where Released = 1
+            order by [Order] desc";
 
             //List<Dictionary<string, Object>>型で取得
             var stories = Con.ExecuteSelect(sql, null);
 
             //Story型に変換してreturn
             var resultStories = new List<Story>();
-            foreach (var dicStory in stories) {
+            foreach (var dicStory in stories)
+            {
                 var story = new Story();
                 story.StoryId = (int)dicStory["StoryId"];
                 story.StoryName = (string)dicStory["StoryName"];
@@ -33,7 +39,8 @@ namespace Z_Apps.Models.Stories.Stories {
             return resultStories;
         }
 
-        public Story GetStory(string storyName) {
+        public Story GetStory(string storyName)
+        {
             //SQL文作成
             string sql = "";
             sql += "select * from tblStoryMst";
@@ -45,14 +52,16 @@ namespace Z_Apps.Models.Stories.Stories {
             })
             .FirstOrDefault();
 
-            if (result == null) {
+            if (result == null)
+            {
                 // 1件もデータがなければ、
                 // フロントから不正なパラメータが来ている可能性があるためエラー
                 throw new Exception();
             }
 
             //Story型に変換してreturn
-            return new Story() {
+            return new Story()
+            {
                 StoryId = (int)result["StoryId"],
                 StoryName = (string)result["StoryName"],
                 Description = (string)result["Description"],
@@ -61,7 +70,8 @@ namespace Z_Apps.Models.Stories.Stories {
             };
         }
 
-        public Story GetStoryById(int storyId) {
+        public Story GetStoryById(int storyId)
+        {
             //SQL文作成
             string sql = "";
             sql += "select * from tblStoryMst";
@@ -72,25 +82,29 @@ namespace Z_Apps.Models.Stories.Stories {
                 { "@storyId", new object[2] { SqlDbType.Int, storyId } }
             }).FirstOrDefault();
 
-            if (result == null) {
+            if (result == null)
+            {
                 return null;
             }
 
             //Story型に変換してreturn
-            return new Story() {
+            return new Story()
+            {
                 StoryId = (int)result["StoryId"],
                 StoryName = (string)result["StoryName"],
                 Description = (string)result["Description"]
             };
         }
 
-        public bool UpdateDesc(int storyId, string storyName, string desc) {
+        public bool UpdateDesc(int storyId, string storyName, string desc)
+        {
             bool result;
             string replacedDesc = desc.Replace("\r", "\n").Replace("\n\n", "\n").Replace("\n\n", "\n")
                 .Replace("\n", "\\n");
 
             string sql = "";
-            if (GetStoryById(storyId) == null) {
+            if (GetStoryById(storyId) == null)
+            {
                 //レコードがなければInsert
                 sql = "";
                 sql += "Insert into tblStoryMst";
@@ -102,7 +116,9 @@ namespace Z_Apps.Models.Stories.Stories {
                     { "@storyId", new object[2] { SqlDbType.Int, storyId }},
                     { "@storyName", new object[2] { SqlDbType.NVarChar, storyName }}
                 });
-            } else {
+            }
+            else
+            {
                 //レコードがあればUpdate
                 sql = "";
                 sql += "update tblStoryMst";
