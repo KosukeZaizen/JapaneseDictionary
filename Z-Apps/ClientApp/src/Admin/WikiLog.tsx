f7ad6877b5b03e63d49cec1fdf56ddfefbb91a32
Kosuke_Zaizen <kosukezaizen@yahoo.co.jp>
2022-02-22 19:47:37 +0900

    show log messages

--- Z-Apps/ClientApp/src/Admin/WikiLog.tsx
+++ Z-Apps/ClientApp/src/Admin/WikiLog.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Collapse } from "reactstrap";
 
 interface WikiLog {
     procType: string;
@@ -11,8 +12,16 @@ interface WikiLog {
 const tdStyle = { border: "solid", padding: 5 };
 const thStyle = { border: "solid", padding: 5, backgroundColor: "ivory" };
 
+type NoIndexLogMessage = {
+    procType: "zApps_dictionary_noIndex";
+    logs: { [key: string]: { length: number; words: string[] } };
+};
+
+type LogMessage = NoIndexLogMessage | null;
+
 export default function WikiLog() {
     const [logs, setLogs] = useState<WikiLog[]>([]);
+    const [message, setMessage] = useState<LogMessage>(null);
 
     useEffect(() => {
         const load = async () => {
@@ -53,12 +62,91 @@ export default function WikiLog() {
                             </td>
                             <td style={tdStyle}>{log.startTime}</td>
                             <td style={tdStyle}>{log.endTime}</td>
-                            <td style={tdStyle}>{log.message}</td>
+                            <td style={tdStyle}>
+                                {log.message && (
+                                    <button
+                                        onClick={() => {
+                                            if (
+                                                typeof log.message !== "string"
+                                            ) {
+                                                return;
+                                            }
+
+                                            switch (log.procType) {
+                                                case "zApps_dictionary_noIndex":
+                                                    setMessage({
+                                                        procType: log.procType,
+                                                        logs: JSON.parse(
+                                                            log.message
+                                                        ),
+                                                    });
+                                                    break;
+                                                default:
+                                                    break;
+                                            }
+                                        }}
+                                    >
+                                        show
+                                    </button>
+                                )}
+                            </td>
                         </tr>
                     ))}
                 </tbody>
             </table>
+            <Message message={message} />
         </>
+    );
+}
+
+function Message({ message }: { message: LogMessage }) {
+    if (!message) {
+        return null;
+    }
+
+    switch (message.procType) {
+        case "zApps_dictionary_noIndex":
+            return <NoIndexMessage message={message} />;
+        default:
+            return null;
+    }
+}
+
+function NoIndexMessage({ message }: { message: NoIndexLogMessage }) {
+    return (
+        <>
+            {Object.keys(message.logs).map(key => (
+                <WordsInfo k={key} key={key} logs={message.logs} />
+            ))}
+        </>
+    );
+}
+
+function WordsInfo({
+    k,
+    logs,
+}: {
+    k: string;
+    logs: NoIndexLogMessage["logs"];
+}) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const val = logs[k];
+    return (
+        <div>
+            {k} length: {val.length}
+            {val.length > 0 && (
+                <button
+                    style={{ marginLeft: 30 }}
+                    onClick={() => {
+                        setIsOpen(!isOpen);
+                    }}
+                >
+                    {isOpen ? "close" : "open"}
+                </button>
+            )}
+            <Collapse isOpen={isOpen}>{val.words.join(", ")}</Collapse>
+        </div>
     );
 }
 
